249e90a3072a4d08a8a58b1f1176185e05aaaca3
mmmG <a331998513@gmail.com>
2021-05-16 04:12:59 +0200

    popup message

--- js/email.js
+++ js/email.js
@@ -3,7 +3,7 @@ import 'https://cdn.jsdelivr.net/npm/emailjs-com@2/dist/email.min.js';
 const trigger = document.querySelector('[data-js="email-box-trigger"]')
 const emailBox = document.querySelector('[data-js="email-box"]')
 const bgTrigger = document.querySelector('[data-js="blur-trigger"]')
-
+const popSuccess = document.querySelector('[data-js="sending-success"]')
 const open = ()=>{
     bgTrigger.classList.add('email--backDrop')
     document.querySelector('.email--backDrop').style.backdropFilter = "blur(2px)"
@@ -38,6 +38,10 @@ export const sendEmail = document.querySelector('form').addEventListener('submit
     emailjs.sendForm('Gmail', 'FromPortfolio', form , 'user_TV8FyBqlJdT9MLqlMZjuh')
         .then(result => {
             console.log('SUCCESS!', result.status, result.text)
+            popSuccess.style.top = "2rem";
+            setTimeout(()=>{
+                popSuccess.style.top = "-10rem";
+            },3000)
         }), error => {
             console.log('FAILED...', error)
         }
