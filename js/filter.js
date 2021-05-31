d21f812eb07a814f1e620c862ad5034b9c3bbc92
mmmG <a331998513@gmail.com>
2021-05-31 09:36:40 +0200

    filter transition fixed

--- js/filter.js
+++ js/filter.js
@@ -3,46 +3,66 @@ const btnAll = filter.querySelector('#all');
 const btnJs = filter.querySelector('#js');
 const btnStyle = filter.querySelector('#styling');
 
-export const portfolioFilter = (event) =>{
+const displayItem = (item)=>{
+    setTimeout(()=>{
+        item.style.display = 'flex';
+        setTimeout(()=>{
+            item.style.opacity = 1;
+        },100)
+    },300);
+}
+const hideItem = (item)=>{
+    item.style.opacity = 0;
+    setTimeout(()=>{
+        item.style.display = 'none'
+    }, 300);
+}
+
+export const portfolioFilter = (e) =>{
+    e.preventDefault();
     const portfolios = document.querySelectorAll('.portfolio__item');
-    portfolios.forEach(item => {
-        switch (event.id){
+    const btn = filter.querySelectorAll('button');
+    const event = e.target;
+    btn.forEach(item=>{
+        switch (event){
             case 'all':
-                item.style.display = 'flex';
-                setTimeout(()=>{
-                    item.style.opacity = 1;
-                    item.style.transform = 'scale(1)';
-                },50)
+                item.id === 'all'
+                ? item.style.color = 'hsl(272, 76%, 53%)'
+                : item.style.color = 'hsl(0, 0%, 22%)';
                 break;
             case 'js':
-                if(item.dataset.id === 'javascript'){
-                    item.style.display = 'flex';
-                    setTimeout(()=>{
-                        item.style.opacity = 1;
-                        item.style.transform = 'scale(1)';
-                    },50);
-                } else {
-                    item.style.opacity = 0;
-                    item.style.transform = 'scale(0)';
-                    setTimeout(()=>item.style.display = 'none', 300);
-                };
+                item.id === 'js'
+                ? item.style.color = 'hsl(272, 76%, 53%)'
+                : item.style.color = 'hsl(0, 0%, 22%)';
                 break;
             case 'styling':
-                if(item.dataset.id === 'styling'){
-                    item.style.display = 'flex';
-                    setTimeout(()=>{
-                        item.style.opacity = 1;
-                        item.style.transform = 'scale(1)';
-                    },50);
-                } else {
-                    item.style.opacity = 0;
-                    item.style.transform = 'scale(0)';
-                    setTimeout(()=>item.style.display = 'none', 300);
-                };
+                item.id === 'styling'
+                ? item.style.color = 'hsl(272, 76%, 53%)'
+                : item.style.color = 'hsl(0, 0%, 22%)';
                 break;
-
+        }
+    })
+    portfolios.forEach(item => {
+        
+        switch (event.id){
+            case 'all':
+                hideItem(item);
+                displayItem(item);
+                break;
+            case 'js':
+                hideItem(item);
+                if(item.dataset.id === 'javascript') displayItem(item)
+                // : hideItem(item);
+                break;
+            case 'styling':
+                hideItem(item);
+                if(item.dataset.id === 'styling') displayItem(item)
+                break;
+            default:
+                hideItem(item);
+                displayItem(item);
         }
     })
 } 
 
-filter.addEventListener('click', (e)=> portfolioFilter(e.target))
+filter.addEventListener('click', (e)=> portfolioFilter(e))
